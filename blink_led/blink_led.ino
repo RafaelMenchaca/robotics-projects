a5c4d88c0cb87049b1fcfe187984d2b053325a2f
RafaelMenchaca <arafael.menchaca@gmail.com>
2025-02-25 20:28:31 -0600

    add 3 leds and make then blink at same time

--- blink_led/blink_led.ino
+++ blink_led/blink_led.ino
@@ -1,17 +1,24 @@
 // Simple Blink LED Program
-int ledPin = 9;  // The built-in LED on most Arduino boards
+int yellow = 9;  // The built-in LED on most Arduino boards
+int red = 8;
+int green = 10;
 
 void setup() {
-    pinMode(ledPin, OUTPUT);
+    pinMode(yellow, OUTPUT);
+    pinMode(red, OUTPUT);
+    pinMode(green, OUTPUT);
     Serial.begin(9600);
     Serial.println("Hello World, Arduino!");
 }
 
 void loop() {
-    digitalWrite(ledPin, HIGH);  // Turn LED on
+    digitalWrite(yellow, HIGH);  // Turn LED on
+    digitalWrite(red, HIGH);
+    digitalWrite(green, HIGH);
     delay(1000);                 // Wait for 1 second
-    digitalWrite(ledPin, LOW);   // Turn LED off
+    digitalWrite(yellow, LOW);   // Turn LED off
+    digitalWrite(red, LOW);
+    digitalWrite(green, LOW);
     delay(1000);                 // Wait for 1 second
-    delay(1000);
     Serial.println("Looping...");
 }
